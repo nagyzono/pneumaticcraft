d78b6efb09f8e5decc0780c28f18427f8b25c916
nagyzono <86381434+nagyzono@users.noreply.github.com>
2024-10-15 00:12:57 +0200

    Update and rename item_test.lua to follow_owner.lua

---
+++ follow_owner.lua
@@ -0,0 +1,18 @@
+function actionWait()
+    while not drone.isActionDone()
+    do
+    sleep(1)
+    end
+end
+
+function goto(x,y,z)
+	drone.addArea(math.floor(x),math.floor(y),math.floor(z))
+	drone.setAction("goto")
+	actionWait()
+	drone.clearArea()
+end
+
+drone = peripheral.wrap("left")
+owner_pos = drone.getVariable($owner_pos)
+
+goto(owner_pos.x,owner_pos.y + 2,owner_pos.z)
