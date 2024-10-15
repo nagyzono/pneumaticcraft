c6e5afaf6547b7830dc6ff6ed2d8d9963f3cf5e1
nagyzono <86381434+nagyzono@users.noreply.github.com>
2024-10-15 10:46:13 +0200

    Update guardDrone_startup.lua

--- guardDrone_startup.lua
+++ guardDrone_startup.lua
@@ -95,7 +95,7 @@ function guard(guard_pos)
 		guard_pos.z + 25,
 		"filled"
 	)
-	drone.showArea()
+	--drone.showArea()
 	--drone.setUseMaxActions(true)
 	--drone.setMaxActions(20)
 	drone.setAction("entity_attack")
